433ce7cb6631c1e0bd902fef0a80cf0660255a1a
Vu Van Nghia 20206205 <nghiavu2k2abc@gmail.com>
2023-02-06 14:21:26 +0700

    t1b3

--- ThucHanhTuan/Tuan1/Tuan1Bai3/SourceCode/Tuan1Bai3/Tuan1Bai3/Program.cs
+++ ThucHanhTuan/Tuan1/Tuan1Bai3/SourceCode/Tuan1Bai3/Tuan1Bai3/Program.cs
@@ -5,21 +5,19 @@ namespace Tuan1Bai3 {
         static void Main(string[] args) {
             Console.OutputEncoding=Encoding.UTF8;
 
-            Console.WriteLine("Chương trình tính tổng từ a đến b:");
+            Console.WriteLine("Chương trình tính tổng từ begin đến end:");
 
-            Console.Write("Nhập giá trị a = ");
-            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Nhập giá trị begin = ");
+            int _begin = Int32.Parse(Console.ReadLine());
 
-            Console.Write("Nhập giá trị b = ");
-            int b = Int32.Parse(Console.ReadLine());
+            Console.Write("Nhập giá trị end = ");
+            int _end = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("\tKết quả: Tổng từ {0} đến {1} là: {2}",a,b,Sum(a,b));
+            Sum _sum = new Sum(_begin,_end);
+            Console.WriteLine(_sum.FindSum());
 
             Console.WriteLine("Nhấn phím bất kì!");
             Console.ReadKey();
-        }
-        public static int Sum(int ibat_dau,int iket_thuc) {
-            return (ibat_dau+iket_thuc)*(iket_thuc-ibat_dau+1)/2;
         }
     }
 }
---
+++ ThucHanhTuan/Tuan1/Tuan1Bai3/SourceCode/Tuan1Bai3/Tuan1Bai3/Sum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuan1Bai3 {
+    class Sum {
+        #region Fields
+        private int begin;
+        private int end;
+        #endregion
+        #region Properties
+        public int Begin { get => begin; set => begin=value; }
+        public int End { get => end; set => end=value; }
+        #endregion
+        #region Constructor
+        public Sum(int iBegin,int iEnd) {
+            begin=iBegin;
+            end=iEnd;
+        }
+        #endregion
+        #region Methods
+        public string FindSum() {
+            int _sum = (Begin+End)*(End-Begin+1)/2;
+            return ($"\tKết quả: Tổng từ {Begin} đến {End} là: {_sum}");
+        }
+        #endregion
+    }
+}
